3011919205e70c764313ede4e06a7f898d359bc4
jcsoftia <ajcarlos032@gmail.com>
2020-04-05 23:22:32 -0500

    testing table cells quatity with parent

--- src/js/api.js
+++ src/js/api.js
@@ -67,7 +67,7 @@ let getall = setInterval(async () => {
     .children("tbody")
     .children("tr")
     .children("td");
-  console.log(countriesTableCells[1].parent.children.length,"cantidad de columnas");
+  console.log((countriesTableCells[1].parent.children.length - 1)/2,"cantidad de columnas");
   // NOTE: this will change when table format change in website
   const totalColumns = 12;
   const countryColIndex = 0;
