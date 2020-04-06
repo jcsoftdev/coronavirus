3099cca98ca73a66f8e173f47f766255fa7f68be
jcsoftia <ajcarlos032@gmail.com>
2020-04-05 23:11:11 -0500

    World_bug

--- src/js/api.js
+++ src/js/api.js
@@ -67,7 +67,7 @@ let getall = setInterval(async () => {
     .children("tbody")
     .children("tr")
     .children("td");
-
+  console.log(countriesTableCells);
   // NOTE: this will change when table format change in website
   const totalColumns = 12;
   const countryColIndex = 0;
